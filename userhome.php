19287097c05ec3c3fb812edf49bd8a4ff5b0c763
bl-op <ffbe9001@gmail.com>
2017-06-04 01:31:39 +0800

    (test)

--- userhome.php
+++ userhome.php
@@ -8,7 +8,7 @@ include("session.php");
 		<title>User Profile</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, intial-scale=1.0">
-		<link rel="stylesheet" href="resources\css\stylesheet.css">
+		<link rel="stylesheet" href="stylesheet.css">
 	</head>
 	<body>
 		<header>
@@ -24,11 +24,13 @@ include("session.php");
 
   <body>
 	  
-	  <div class="profilecontainer">
-		<h1>User Profile</h1>
-	  </div>
-		<p style="text-align: center">Name</p><br><br>
-	  	<?php echo $row['firstname']." ".$row['lastname']; ?>
+	  	<div class="profilecontainer">
+			<h1 style="text-align: center">User Profile</h1>
+
+			<p style="text-align: center">About Me<br><br>
+			Hello <?php echo $row['firstname']." ".$row['lastname']; ?>! Currently displaying your user profile details.</p>
+
+		</div>
 
   </body>
 
