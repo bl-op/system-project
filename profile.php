5b8364b6a62245b82d74088781c995dfa61b4b7f
bl-op <ffbe9001@gmail.com>
2017-06-04 05:37:38 +0800

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,18 @@
+<?php
+ob_start();
+session_start();
+
+$id = (isset($_GET['id'])) ? $_GET['id'] : $_SESSION['id'];
+$query = "SELECT * FROM cfusers WHERE userID = $id";
+
+// if session is not set this will redirect to userhome page
+if( !isset($_SESSION['id']) ) {
+	header("Location:userhome.php?id=" . $_SESSION['id']);
+ 	exit;
+}
+
+// select logged in users detail
+$result = mysql_query("SELECT * FROM cfusers WHERE userID=".$_SESSION['id']);
+$row = mysql_fetch_array($result);
+$id = $_GET['userID'];
+?>
