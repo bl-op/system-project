52cd0c2d6c389c0ede1f2269b783e300abbaf9c6
bl-op <ffbe9001@gmail.com>
2017-06-04 00:52:56 +0800

    (test) changes
    correction

--- userprofile.php
+++ userprofile.php
@@ -1,37 +1,24 @@
 <?php
 
-$dbhost = 'localhost';
- $dbuser = 'root';
- $dbpass = '';
- $conn = mysql_connect($dbhost, $dbuser, $dbpass);
- 
- if($conn ) {
-    echo "Connected successfully.<br>";
-    } else {
-        die('Could not connect: ' . mysql_error());
-    }
-         
-        mysql_select_db('Carifund');
-
 function getUsersData($id)
 {
     $array = array();
-    $q = mysql_query("SELECT * FROM `cfusers` WHERE `id`=".$id);
-    while($r = mysql_fetch_assoc($q))
+    $query = mysql_query("SELECT * FROM cfusers WHERE id=".$id);
+    while($row = mysql_fetch_assoc($result))
     {
-        $array['id'] = $r['id'];
-        $array['firstname'] = $r['firstname'];
-        $array['lastname'] = $r['lastname'];
+        $array['id'] = $row['id'];
+        $array['firstname'] = $row['firstname'];
+        $array['lastname'] = $row['lastname'];
     }
     return $array;
 }
 
 function getId($username)
 {
-    $q = mysql_query("SELECT `id` FROM `cfusers` WHERE `username`='".$username."'");
-    while($r = mysql_fetch_assoc($q))
+    $query = mysql_query("SELECT `id` FROM `cfusers` WHERE `username`='".$username."'");
+    while($row = mysql_fetch_assoc($result))
     {
-        return $r['id'];
+        return $row['id'];
     }
 }
 
