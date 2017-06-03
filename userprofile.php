cc1f4b5c8fa6552048ae11e929c8a4cbe627965c
bl-op <ffbe9001@gmail.com>
2017-06-04 00:30:05 +0800

    (test) made changes to the db connections

--- userprofile.php
+++ userprofile.php
@@ -1,6 +1,17 @@
 <?php
 
-require("dbc.php");
+$dbhost = 'localhost';
+ $dbuser = 'root';
+ $dbpass = '';
+ $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+ 
+ if($conn ) {
+    echo "Connected successfully.<br>";
+    } else {
+        die('Could not connect: ' . mysql_error());
+    }
+         
+        mysql_select_db('Carifund');
 
 function getUsersData($id)
 {
