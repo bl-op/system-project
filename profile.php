6caa4b9c8714e7022e0d4363ab0b75963fd0499f
bl-op <ffbe9001@gmail.com>
2017-06-05 20:44:51 +0800

    changes to profile details

--- profile.php
+++ profile.php
@@ -19,7 +19,7 @@
 
 <html>
     <head>
-        <title><?php echo $result[0]["firstName"]?></title>
+        <title><?php echo $result[0]["firstName"]?>'s User Profile</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="resources\css\stylesheet.css">
